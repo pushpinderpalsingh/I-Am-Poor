644cb38d93215a67280a1612aa3d450de71ad415
Pushpinder Pal Singh <thisisme@pushpinderpalsingh.com>
2019-10-24 10:25:16 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Pushpinder Pal Singh on 24/10/19.
+//  Copyright © 2019 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
